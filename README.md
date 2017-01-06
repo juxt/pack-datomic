5530e1b632f02d4bf7739d627c07f97f080fa72e
Jon Pither <jon.pither@gmail.com>
2017-01-06 11:32:56 +0000

    Initial Datomic package configuration

---
+++ README.md
@@ -0,0 +1,11 @@
+# Packer-Datomic
+
+Install Packer.
+
+Then replace the ? symbols below and run.
+
+`packer build -var 'datomic_version=?' -var 'datomic_user=?' -var 'datomic_password=? datomic.json`
+
+# Overview
+
+The bit we want to pre-bake is the installation and machine configuration prior to running Datomic.
---
+++ install-datomic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+apt-get install unzip
+
+mkdir /datomic
+cd /datomic
+
+apt-get update
+apt-get -y install default-jre
+apt-get -y install default-jdk
+
+wget --http-user=$DATOMIC_USER --http-password=$DATOMIC_PASSWORD "https://my.datomic.com/repo/com/datomic/datomic-pro/$DATOMIC_VERSION/datomic-pro-$DATOMIC_VERSION.zip" -O "datomic-pro-$DATOMIC_VERSION.zip"
+unzip "datomic-pro-$DATOMIC_VERSION.zip"
