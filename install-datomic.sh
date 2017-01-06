b2df208fd83bbc901e4f5ba687dfb64a88547600
Jon Pither <jon.pither@gmail.com>
2017-01-06 16:08:28 +0000

    Changes logs output

--- install-datomic.sh
+++ install-datomic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+exec > >(tee /var/log/packer-data.log|logger -t packer-data -s 2>/dev/console) 2>&1
 
 apt-get install unzip
 
